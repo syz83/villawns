a14129e70852bcb97f77ca9d35f545d2dac7df87
syz83 <stevenzh2012@gmail.com>
2014-07-02 16:08:14 -0700

    changed links to minyawns.com

--- test/wp-content/themes/minyawns/templates/_jobs.php
+++ test/wp-content/themes/minyawns/templates/_jobs.php
@@ -262,7 +262,7 @@ $current_user_role =  trim($user_role);
 							<div class="msg-info">
 							<span>Please Note</span>
 							Funds to minions will be released on job completion only. If minions don't show up, you will get full refund.
-<br> Any credit or debit card will do!</div>
+<br> Any credit or debit card will do! Steven was here</div>
                         <span id="paypal_pay" style="display:none"><a href="#paymentform"  data-toggle="modal"><input type="image" width="160" height="40" src="<?php echo site_url()."/wp-content/themes/minyawns/images/pay-btn.png"; ?>" value="Pay with PayPal" class="center-image"/></a></span>
                      <span id="selection_message"></span>
                                     </div>
